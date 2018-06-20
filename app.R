5c401316db3eabe7b196da58d84c4c9bf43fdf1d
anser0 <39662526+anser0@users.noreply.github.com>
2018-06-20 13:27:39 -0500

    Update app.R

--- app.R
+++ app.R
@@ -34,8 +34,7 @@ server <- function(input, output, session) {
 ui <- navbarPage(
   "Debate SPYDER",
   tabPanel("Home", fillPage(
-    h1("Welcome to",
-       span("Debate SPYDER", style = "color:red"),
+    h1(span("Debate SPYDER", style = "color:red"),
        align = "center"),
     h3(
       "A website dedicated to helping you",
@@ -50,8 +49,8 @@ ui <- navbarPage(
       align = "center"
     ),
     img(
-      src = "SPYDER.PNG",
-      style="display: block; margin-left: auto; margin-right: auto;"
+      src = "https://cdn.samyok.us/SPYDER.png",
+      style="display: block; margin-left: auto; margin-right: auto; width=100%"
     ),
     h4("Credits:")
   )),
@@ -60,25 +59,25 @@ ui <- navbarPage(
     tabPanel("Schools",
              textInput("schoolsearch", label = "School Search"),
              actionButton("schoolsearchbutton", label = "Go")
-             ),
-    tabPanel("Cities", 
-             textInput("citysearch", label = "City Search"),
-             actionButton("citysearchbutton", label = "Go")
-             ),
-    tabPanel("Districts", 
-             textInput("districtsearch", label = "District Search"),
-             actionButton("districtsearchbutton", label = "Go")
-             ),
-    tabPanel("States", 
-             textInput("statesearch", label = "State Search"),
-             actionButton("statesearchbutton", label = "Go")
-             ),
-    tabPanel("People", 
-             textInput("peoplesearch", label = "People Search"),
-             actionButton("peoplesearchbutton", label = "Go")
-             )
+    ),
+    tabPanel("Competition", 
+             textInput("competitionsearch", label = "Competition Search"),
+             actionButton("competitionsearchbutton", label = "Go")
+    ),
+    tabPanel("Individuals", 
+             textInput("individualsearch", label = "Individual Search"),
+             actionButton("individualsearchbutton", label = "Go")
+    )
   ),
-  tabPanel("Info", "INFORMATION"),
+  tabPanel("Info",
+           h1("Information"),
+           h3(span("Schools", style="color:red")),
+           p("View any school's details and statistics.", style="font-size:20px" ),
+           h3(span("Competition", style="color:red")), 
+           p("Copy and paste the list of your competitors to analyze all of their statistics at once, including state, district, school, and points.",  style="font-size:20px" ),
+           h3(span("Individuals", style = "color:red")),
+           p("Search for any individual and view details and statistics.",  style="font-size:20px")
+           ),
   tabPanel(
     textOutput("log_text"),
     inline = TRUE,
@@ -91,7 +90,6 @@ ui <- navbarPage(
   collapsible = TRUE
   
 )
-# p('123')
 
 # Run the app ----
 shinyApp(ui = ui, server = server
