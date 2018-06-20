a2c6cbf3629c339e1fbfc5408155743bfddb1833
AnyaOlson <40405818+AnyaOlson@users.noreply.github.com>
2018-06-19 21:27:54 -0700

    Update app.R

--- app.R
+++ app.R
@@ -1,3 +1,22 @@
+# library(shiny)
+# 
+# # Define UI ----
+# ui <- fluidPage(
+# 
+# 
+#   
+#   titlePanel("Debate SPYDER"),
+#   
+#   navlistPanel(
+#     "Data",
+#     tabPanel("Schools"),
+#     tabPanel("Cities"),
+#     tabPanel("Districts"),
+#     tabPanel("States"),
+#     tabPanel("People")
+#     
+#   )
+# )
 library(shiny)
 
 # Define server logic ----
@@ -7,20 +26,25 @@ server <- function(input, output, session) {
 
 ui <- fluidPage(
   titlePanel("Debate SPYDER"),
-sidebarLayout(
-  sidebarPanel("sidebar panel"),
-  mainPanel(
-    h1("Welcome to",
-      span("Debate SPYDER", style="color:red"),
-      align = "center"),
-    h3("A website dedicated to helping you",
-      span("analyze" ,style="color:red"), "and", span("interpret", style="color:red"), "debate data",
-      align = "center"),
-    h6("If you know the opponent and know yourself, you need not fear the result of a hundred debate rounds. - Sun Tzu, modified", align = "center"),
-    img(src="SPYDER.PNG", height = 600, width = 600, align="center"),
-    h4("Credits:")
+  navlistPanel(
+    "Data",
+    tabPanel("Schools"),
+    tabPanel("Cities"),
+    tabPanel("Districts"),
+    tabPanel("States"),
+    tabPanel("People"),
+    mainPanel(
+      h1("Welcome to",
+         span("Debate SPYDER", style="color:red"),
+         align = "center"),
+      h3("A website dedicated to helping you",
+         span("analyze" ,style="color:red"), "and", span("interpret", style="color:red"), "debate data",
+         align = "center"),
+      h6("If you know the opponent and know yourself, you need not fear the result of a hundred debate rounds. - Sun Tzu, modified", align = "center"),
+      img(src="SPYDER.PNG", height = 600, width = 600, align="center"),
+      h4("Credits:")
     )
-)
+  )
 )
 # p('123')
 
